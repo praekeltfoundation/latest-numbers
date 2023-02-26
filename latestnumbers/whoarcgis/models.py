4e4a0d16c87ea3e490b9525d921b758228e12461
Codie Roelf <codiebeulaine@gmail.com>
2023-02-26 17:29:25 +0200

    initial

---
+++ latestnumbers/whoarcgis/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Statistic(models.Model):
+    country_code = models.CharField(max_length=10)
+    country_code_2 = models.CharField(max_length=2, blank=True, null=True) #Iso - 2
+    updated_at = models.DateTimeField()
+    new_cases = models.IntegerField(default=0)
+    cum_cases = models.IntegerField(default=0)
+    new_deaths = models.IntegerField(default=0)
+    cum_deaths = models.IntegerField(default=0)
+    total_vaccinations = models.IntegerField(default=0)
---
+++ latestnumbers/whoarcgis/views.py
@@ -0,0 +1,18 @@
+from django.http import JsonResponse, HttpResponse
+from .utils import retrieve_country_stats_from_arcgis, retrieve_global_stats_from_arcgis, retrieve_latest_news
+
+
+def get_stats_country(request):
+    country_code = request.GET.get("country_code")
+    statistic = retrieve_country_stats_from_arcgis(country_code)
+    return JsonResponse(statistic)
+
+
+def get_stats_global(request):
+    statistic = retrieve_global_stats_from_arcgis()
+    return JsonResponse(statistic)
+
+
+def get_latest_news(request):
+    statistic = retrieve_latest_news()
+    return HttpResponse(statistic)
